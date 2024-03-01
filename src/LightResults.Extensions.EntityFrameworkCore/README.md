08d1ccf53effbec85567fbdf1c2206d66bd0ff84
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2024-02-29 19:15:12 -0500

    Updated exception handling. (#3)

--- src/LightResults.Extensions.EntityFrameworkCore/README.md
+++ src/LightResults.Extensions.EntityFrameworkCore/README.md
@@ -4,7 +4,7 @@
 
 Extensions for [LightResults](https://github.com/jscarle/LightResults), an extremely light and modern Operation Result Pattern library for .NET.
 
-## LightResults.Extensions.EntityFrameworkCore
+## EntityFrameworkCore
 
 Provides a DbContext that wraps context operations using LightResults.
 
---
+++ src/LightResults.Extensions.ExceptionHandling/ExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace LightResults.Extensions.ExceptionHandling;
+
+/// <summary>Provides extension methods for handling exceptions using the <see cref="Result" /> type.</summary>
+public static partial class ExceptionHandler
+{
+    private const string ExceptionKey = "Exception";
+
+    private static Result HandleException(Exception ex)
+    {
+        var error = GetExceptionError(ex);
+        return Result.Fail(error);
+    }
+
+    private static Result<TResult> HandleException<TResult>(Exception ex)
+    {
+        var error = GetExceptionError(ex);
+        return Result<TResult>.Fail(error);
+    }
+
+    private static Error GetExceptionError(Exception ex)
+    {
+        var message = GetExceptionMessage(ex);
+        var error = new Error(message, (ExceptionKey, ex));
+        return error;
+    }
+
+    private static string GetExceptionMessage(Exception ex)
+    {
+        return $"{ex.GetType().Name}: {ex.Message}";
+    }
+}
