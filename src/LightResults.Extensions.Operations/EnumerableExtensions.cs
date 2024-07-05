47c121e516c5dc6884d47f0bc6353329bbed0d12
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2024-07-05 01:18:36 -0400

    Improvements?

--- src/LightResults.Extensions.Operations/EnumerableExtensions.cs
+++ src/LightResults.Extensions.Operations/EnumerableExtensions.cs
@@ -1,45 +1,44 @@
 ﻿namespace LightResults.Extensions.Operations;
 
-/// <summary>
-/// Extension methods for <see cref="IEnumerable{T}"/>.
-/// </summary>
+/// <summary>Extension methods for <see cref="IEnumerable{T}"/>.</summary>
 public static class EnumerableExtensions
 {
-    /// <summary>
-    /// Combine multiple results into a single result.
-    /// </summary>
+    /// <summary>Combine multiple results into a single result.</summary>
     /// <param name="results">The results to combine.</param>
     /// <returns>
-    /// A new successful result if all <paramref name="results"/> were successful;
-    /// otherwise a new failed result with all errors found in the <paramref name="results"/>.
+    /// A new successful result if all <paramref name="results"/> were successful; otherwise a new failed result with all errors found in the
+    /// <paramref name="results"/>.
     /// </returns>
     /// <remarks>This will enumerate the <paramref name="results"/>.</remarks>
     public static Result Collect(this IEnumerable<Result> results)
     {
-        var resultsArray = results as Result[] ?? results.ToArray();
-        // Spans are faster to iterate over than arrays
-        ReadOnlySpan<Result> resultsSpan = resultsArray;
-        var errors = new List<IError>(resultsSpan.Length);
+        var errors = results.SelectMany(x => x.Errors)
+            .ToList();
 
-        for (var i = 0; i < resultsSpan.Length; i++)
-        {
-            if (resultsSpan[i].IsFailed())
-                errors.AddRange(resultsSpan[i].Errors);
-        }
-
-        return errors.Count == 0
-            ? Result.Ok()
-            : Result.Fail(errors);
+        return errors.Count == 0 ? Result.Ok() : Result.Fail(errors);
     }
 
-    /// <summary>
-    /// Combine multiple results into a single result.
-    /// </summary>
+    /// <summary>Combine multiple results into a single result.</summary>
+    /// <param name="results">The results to combine.</param>
+    /// <returns>
+    /// A new successful result if all <paramref name="results"/> were successful; otherwise a new failed result with all errors found in the
+    /// <paramref name="results"/>.
+    /// </returns>
+    /// <remarks>This will enumerate the <paramref name="results"/>.</remarks>
+    public static Result Collect(this IReadOnlyList<Result> results)
+    {
+        var errors = results.SelectMany(x => x.Errors)
+            .ToList();
+
+        return errors.Count == 0 ? Result.Ok() : Result.Fail(errors);
+    }
+
+    /// <summary>Combine multiple results into a single result.</summary>
     /// <param name="results">The results to combine.</param>
     /// <typeparam name="TValue">The type of the value of the result.</typeparam>
     /// <returns>
-    /// A new result containing all the values of the <paramref name="results"/> if they were successful;
-    /// otherwise a failed result with all errors found in the <paramref name="results"/>.
+    /// A new result containing all the values of the <paramref name="results"/> if they were successful; otherwise a failed result with all errors found in
+    /// the <paramref name="results"/>.
     /// </returns>
     /// <remarks>This will enumerate the <paramref name="results"/>.</remarks>
     public static Result<IReadOnlyList<TValue>> Collect<TValue>(this IEnumerable<Result<TValue>> results)
@@ -52,14 +51,41 @@ public static class EnumerableExtensions
 
         for (var i = 0; i < resultsSpan.Length; i++)
         {
-            if (resultsSpan[i].IsSuccess(out var value))
+            if (resultsSpan[i]
+                .IsSuccess(out var value))
                 values.Add(value);
             else
                 errors.AddRange(resultsSpan[i].Errors);
         }
 
-        return errors.Count == 0
-            ? Result.Ok<IReadOnlyList<TValue>>(values)
-            : Result.Fail<IReadOnlyList<TValue>>(errors);
+        return errors.Count == 0 ? Result.Ok<IReadOnlyList<TValue>>(values) : Result.Fail<IReadOnlyList<TValue>>(errors);
+    }
+
+    /// <summary>Combine multiple results into a single result.</summary>
+    /// <param name="results">The results to combine.</param>
+    /// <typeparam name="TValue">The type of the value of the result.</typeparam>
+    /// <returns>
+    /// A new result containing all the values of the <paramref name="results"/> if they were successful; otherwise a failed result with all errors found in
+    /// the <paramref name="results"/>.
+    /// </returns>
+    /// <remarks>This will enumerate the <paramref name="results"/>.</remarks>
+    public static Result<IReadOnlyList<TValue>> Collect<TValue>(this IReadOnlyList<Result<TValue>> results)
+    {
+        var resultsArray = results as Result<TValue>[] ?? results.ToArray();
+        // Spans are faster to iterate over than arrays
+        ReadOnlySpan<Result<TValue>> resultsSpan = resultsArray;
+        var values = new List<TValue>(resultsSpan.Length);
+        var errors = new List<IError>(resultsSpan.Length);
+
+        for (var i = 0; i < resultsSpan.Length; i++)
+        {
+            if (resultsSpan[i]
+                .IsSuccess(out var value))
+                values.Add(value);
+            else
+                errors.AddRange(resultsSpan[i].Errors);
+        }
+
+        return errors.Count == 0 ? Result.Ok<IReadOnlyList<TValue>>(values) : Result.Fail<IReadOnlyList<TValue>>(errors);
     }
 }
---
+++ tools/Benchmarks/Benchmarks.cs
@@ -0,0 +1,76 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Jobs;
+using JetBrains.Annotations;
+using LightResults;
+using LightResults.Extensions.Operations;
+
+namespace Benchmarks;
+
+// ReSharper disable RedundantTypeArgumentsOfMethod
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net90)]
+[IterationTime(250)]
+[HideColumns(Column.Job, Column.Iterations, Column.Error, Column.StdDev, Column.Median, Column.RatioSD, Column.Gen0, Column.Gen1, Column.Gen2)]
+public class Benchmarks
+{
+    private const int Seed = 42;
+    private IEnumerable<Result> _enumerable = null!;
+    private IEnumerable<Result<string>> _enumerableT = null!;
+    private IReadOnlyList<Result> _readOnlyList = null!;
+    private IReadOnlyList<Result<string>> _readOnlyListT = null!;
+
+    [UsedImplicitly]
+    [Params(10, 100, 1000, 10000)]
+    public int Iterations { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var random = new Random(Seed);
+
+        var enumerable = new List<Result>();
+        for (var index = 0; index < Iterations; index++)
+            enumerable.Add(random.Next(0, 1) == 0 ? Result.Ok() : Result.Fail(index.ToString()));
+        _enumerable = enumerable.AsEnumerable();
+
+        var readOnlyList = new List<Result>();
+        for (var index = 0; index < Iterations; index++)
+            readOnlyList.Add(random.Next(0, 1) == 0 ? Result.Ok() : Result.Fail(index.ToString()));
+        _readOnlyList = readOnlyList;
+
+        var enumerableT = new List<Result<string>>();
+        for (var index = 0; index < Iterations; index++)
+            enumerableT.Add(random.Next(0, 1) == 0 ? Result.Ok(index.ToString()) : Result.Fail<string>(index.ToString()));
+        _enumerableT = enumerableT.AsEnumerable();
+
+        var readOnlyListT = new List<Result<string>>();
+        for (var index = 0; index < Iterations; index++)
+            readOnlyListT.Add(random.Next(0, 1) == 0 ? Result.Ok(index.ToString()) : Result.Fail<string>(index.ToString()));
+        _readOnlyListT = readOnlyListT;
+    }
+
+    [Benchmark]
+    public Result Result_Collect_WithEnumerable()
+    {
+        return _enumerable.Collect();
+    }
+
+    [Benchmark]
+    public Result Result_Collect_WithReadOnlyList()
+    {
+        return _readOnlyList.Collect();
+    }
+
+    [Benchmark]
+    public Result<IReadOnlyList<string>> ResultT_Collect_WithEnumerable()
+    {
+        return _enumerableT.Collect();
+    }
+
+    [Benchmark]
+    public Result<IReadOnlyList<string>> ResultT_Collect_WithReadOnlyList()
+    {
+        return _readOnlyListT.Collect();
+    }
+}
